b8aeff1cbd359e34f71e5f2ca078772475240b9a
Ankita Makade <78643106+Ankita196@users.noreply.github.com>
2021-09-21 11:56:06 +0530

    done

---
+++ 1b.js
@@ -0,0 +1,16 @@
+const paragraph =
+  "I love teaching. If you do not love teaching what else can you love. I love JavaScript if you do not love something which can give life to your application what else can you love.";
+
+function countWords(paragraph, w1, w2) {
+  let array = paragraph.replace(/[.]/g, "").split(" ");
+
+  let word1 = array.filter((x) => x == w1).length;
+
+  let word2 = array.filter((x) => x == w2).length;
+  if (word1 > word2) {
+    console.log("the word  more frequently occured than you");
+  } else {
+    console.log("the word you more frequently occured than love");
+  }
+}
+console.log(countWords(paragraph, "I", "do"));
