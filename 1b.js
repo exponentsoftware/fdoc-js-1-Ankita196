177f34147cf66d2939db93187678fce7f427c796
Ankita Makade <78643106+Ankita196@users.noreply.github.com>
2021-09-21 11:56:53 +0530

    done

--- 1b.js
+++ 1b.js
@@ -1,16 +1,9 @@
-const paragraph =
-  "I love teaching. If you do not love teaching what else can you love. I love JavaScript if you do not love something which can give life to your application what else can you love.";
+const sentence =
+  "%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?";
 
-function countWords(paragraph, w1, w2) {
-  let array = paragraph.replace(/[.]/g, "").split(" ");
-
-  let word1 = array.filter((x) => x == w1).length;
-
-  let word2 = array.filter((x) => x == w2).length;
-  if (word1 > word2) {
-    console.log("the word  more frequently occured than you");
-  } else {
-    console.log("the word you more frequently occured than love");
-  }
+function cleanText(sentence) {
+  let output = sentence.replace(/[^a-zA-Z. ]/g, "");
+  return output;
 }
-console.log(countWords(paragraph, "I", "do"));
+
+console.log(cleanText(sentence));
